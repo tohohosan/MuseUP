2dce65bf2ee6d9e8509c7ca783f10f136f802a8e
tohohosan <kuppyramune315@gmail.com>
2024-11-09 00:02:22 +0900

    post museum

---
+++ db/migrate/20241108085914_create_notes.rb
@@ -0,0 +1,11 @@
+class CreateNotes < ActiveRecord::Migration[7.2]
+  def change
+    create_table :notes do |t|
+      t.text :content
+      t.references :user, null: false, foreign_key: true
+      t.references :museum, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
