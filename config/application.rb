73b328f4a82125a81c38e47b919810851f28a38a
tohohosan <kuppyramune315@gmail.com>
2024-11-21 14:11:16 +0900

    modify view

--- config/application.rb
+++ config/application.rb
@@ -8,21 +8,11 @@ Bundler.require(*Rails.groups)
 
 module MuseUp
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.2
     config.i18n.default_locale = :ja
+    config.time_zone = "Tokyo"
 
-    # Please, add to the `ignore` list any other `lib` subdirectories that do
-    # not contain `.rb` files, or that should not be reloaded or eager loaded.
-    # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
-
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    config.active_record.default_timezone = :local
   end
 end
