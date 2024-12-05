6383ce767c72b232838c246e131f83ae4d5e6de0
tohohosan <kuppyramune315@gmail.com>
2024-12-05 20:05:14 +0900

    modify admin

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -6,12 +6,4 @@ bundle exec rails assets:clean
 bundle exec rails db:migrate
 
 echo "Updating user role for admin user..."
-bundle exec rails runner <<EOF
-user = User.find_by(email: "kuppyramune315@gmail.com")
-if user
-    user.update(role: "admin")
-    puts "Admin role successfully assigned to user with email: #{user.email}"
-else
-    puts "User not found with email: kuppyramune315@gmail.com"
-end
-EOF
+bundle exec rails runner "load 'db/scripts/assign_admin_role.rb'"
