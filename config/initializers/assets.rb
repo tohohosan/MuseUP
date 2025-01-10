0c0d582d43715bb17cbf2b1b37a764893a13cb40
tohohosan <kuppyramune315@gmail.com>
2025-01-10 18:34:08 +0900

    エラーのビューファイルを修正

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,6 +12,8 @@ Rails.application.config.assets.version = "1.0"
 # Rails.application.config.assets.precompile += %w[ admin.js admin.css ]
 Rails.application.config.assets.paths << Rails.root.join("node_modules/@fortawesome/fontawesome-free/webfonts")
 
+Rails.application.config.assets.precompile += %w[favicon.ico]
+
 # config/initializers/assets.rb
 Rails.application.config.assets.paths += [
     Rails.root.join("node_modules"),
