5850e265c149cdca02f97de3695a709481b417b0
tohohosan <kuppyramune315@gmail.com>
2025-02-03 15:59:58 +0900

    JavaScript の整理

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,4 +7,4 @@ pin_all_from "app/javascript/controllers", under: "controllers"
 
 pin "place_autocomplete", to: "place_autocomplete.js"
 pin "@rails/ujs", to: "https://ga.jspm.io/npm:@rails/ujs@7.0.5/lib/assets/compiled/rails-ujs.js"
-pin "museum_maps", to: "museum_maps.js", preload: true
+pin "museum_maps", to: "museum_maps.js"
